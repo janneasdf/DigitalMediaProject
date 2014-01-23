370df3f90bc6b94c0619030af0a926ffb4120d8e
Janne Gr;ndahl <janneasdf@paivola.fi>
2014-01-24 01:00:28 +0200

    Add shader selection panel functionality

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,18 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 $('body').append(renderer.domElement);
 
+// Shaders
+var vertexShaders = {
+  vshader1: $('#vertexShader1').html(),
+  vshader2: $('#vertexShader2').html()
+};
+
+var fragmentShaders = {
+  fshader1: $('#fragmentShader1').html(),
+  fshader2: $('#fragmentShader2').html()
+};
+
+// Mesh
 var geometry = new THREE.CubeGeometry(1,1,1);
 var uniforms = {
   time: { type: "f", value: 1.0 }, 
@@ -15,8 +27,8 @@ var uniforms = {
 };
 var material = new THREE.ShaderMaterial({ 
   uniforms: uniforms, 
-  vertexShader: $('#vertexShader').html(),
-  fragmentShader: $('#fragmentShader').html()
+  vertexShader: vertexShaders['vshader1'],
+  fragmentShader: fragmentShaders['fshader1']
 });
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
@@ -33,9 +45,13 @@ function render() {
 render();
 
 function onVertexShaderChange() {
-  
+  var new_shader_name = $('#vertexShaderSelect').val();
+  material.vertexShader = vertexShaders[new_shader_name];
+  material.needsUpdate = true;
 }
 
 function onFragmentShaderChange() {
-  
+  var new_shader_name = $('#fragmentShaderSelect').val();
+  material.fragmentShader = fragmentShaders[new_shader_name];
+  material.needsUpdate = true;
 }
