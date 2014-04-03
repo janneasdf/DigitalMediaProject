6a4010c3d271406a10022622da1cbb28a85d9475
Janne Gröndahl <janne.grondahl@aalto.fi>
2014-04-03 04:30:59 +0300

    Add demo base stuff

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,7 +2,6 @@
 var width = 400;
 var height = 400;
 
-var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
@@ -36,18 +35,44 @@ var material = new THREE.ShaderMaterial({
   fragmentShader: fragmentShaders['fshader1']
 });
 var model = new THREE.Mesh(geometry, material);
-scene.add(model);
 
 camera.position.z = 5;
 
-var beginTime = (new Date()).getMilliseconds();
+var beginTime = (new Date()).getTime();
 var time = 0;
+
+var demo1 = {};
+var demo2 = {};
+var demo1_inited = false;
+var demo2_inited = false;
+
+var scene = {};
 
 /* Rendering loop */
 function render() {
-  time += (new Date()).getMilliseconds() - beginTime;
-
+  time = (new Date()).getTime() - beginTime;
   uniforms.time.value = time;
+
+  if (time < 20000.0 / 10) {
+    if (!demo1_inited) {  // Init
+      demo1_inited = true;
+      scene = new THREE.Scene();
+      scene.add(model);
+    }
+
+
+  } else if (time < 40000.0) {
+    if (!demo2_inited) {  // Init
+      demo2_inited = true;
+      // Clear scene
+      for (var c in scene.children)
+        scene.remove(c);
+      var model1 = new THREE.Mesh(new THREE.SphereGeometry(1), material);
+      scene.add(model1);
+    }
+
+
+  }
 
   requestAnimationFrame(render);
   renderer.render(scene, camera);
