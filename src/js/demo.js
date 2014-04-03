7942e6b92a459ecb0202b1048cea088b6095a253
Janne Gröndahl <janne.grondahl@aalto.fi>
2014-04-03 18:13:20 +0300

    Add test demo thing that will be removed later

--- src/js/demo.js
+++ src/js/demo.js
@@ -59,19 +59,40 @@ function render() {
   time = (new Date()).getTime() - beginTime;
   uniforms.time.value = time;
 
-  if (time < 20000.0 / 10) {
+  if (time < 20000.0) {
     if (!demo1_inited) {  // Init
       demo1_inited = true;
+	  demo1.numObjects = 10000;
       scene = new THREE.Scene();
-	  	  
-	  var geom = new THREE.SphereGeometry(1);
-	  for (var i = 0; i < 1000; i++) {
-		demo1['sphere' + i] = new THREE.Mesh(geom, material);
-		scene.add(demo1['sphere' + i]);
-		demo1['sphere' + i].position.x = i * 0.1;
+	  
+	  var cube_size = 0.1;
+	  var geom = new THREE.CubeGeometry(cube_size, cube_size, cube_size);
+	  var mat = new THREE.MeshBasicMaterial({ 'color': 0xff0000 });
+	  var scene_size = 50;
+	  for (var i = 0; i < demo1.numObjects; i++) {
+		demo1['object' + i] = new THREE.Mesh(geom, mat);
+		scene.add(demo1['object' + i]);
+		var r = Math.random();
+		demo1['object' + i].position.x = (-1 + 2 * r) * scene_size;
+		r = Math.random();
+		demo1['object' + i].position.y = (-1 + 2 * r) * scene_size;
+		r = Math.random();
+		demo1['object' + i].position.z = (-1 + 2 * r) * scene_size;
 	  }
 	  
     }
+	// Update
+	for (var i = 0; i < demo1.numObjects; i++) {
+	    var rem = i % 3;
+	    if (rem === 0) {
+			demo1['object' + i].rotation.x += 0.05;
+	    } else if (rem === 1) {
+			demo1['object' + i].rotation.y += 0.05;
+	    } else {
+			demo1['object' + i].rotation.z += 0.05;
+	    }
+	
+	}
 
   } else if (time < 40000.0) {
     if (!demo2_inited) {  // Init
